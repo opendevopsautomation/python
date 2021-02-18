997c51120e025ed2f86a34dded7410d35f2e4838
vikas sharma <vikas.prabhakar25@gmail.com>
2021-02-19 00:00:38 +0530

    413. Arithmetic Slices 2nd method

--- leetcode/Arithmetic-Slices.py
+++ leetcode/Arithmetic-Slices.py
@@ -13,3 +13,19 @@ class Solution:
         return result
 #TC: O(n)
 #SC: O(1)
+
+#2nd Method
+class Solution:
+    def numberOfArithmeticSlices(self, A: List[int]) -> int:
+        count=[0]*len(A)
+        result=0
+        print(count)
+        for i in range(2,len(A)):
+            if (A[i]-A[i-1])==(A[i-1]-A[i-2]):
+                count[i]=count[i-1]+1
+            else:
+                count[i]=0
+            result+=count[i]
+        return result
+#TC: O(n)
+#SC: O(n)        
