0d5c357fafd11d573ecfdc7dfdc1be63a9553f5b
vikas sharma <vikas.prabhakar25@gmail.com>
2021-02-18 23:29:03 +0530

    413. Arithmetic Slices

---
+++ Arithmetic-Slices.py
@@ -0,0 +1,15 @@
+#Arithmetic Formula >> A,B,C (B-C= B-A)
+class Solution:
+    def numberOfArithmeticSlices(self, A: List[int]) -> int:
+        count=0
+        result=0
+        for i in range(2,len(A)):
+            print(i)
+            if (A[i]-A[i-1])==(A[i-1]-A[i-2]):
+                count+=1
+            else:
+                count=0
+            result+=count
+        return result
+#TC: O(n)
+#SC: O(1)
