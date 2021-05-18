dfa99a55ca8f8028a0522de0425c2b5cb52df77d
vikas sharma <vikas.prabhakar25@gmail.com>
2021-05-18 22:54:13 +0530

    609. Find Duplicate File in System

--- leetcode/Find-Duplicate-File-in-System.py
+++ leetcode/Find-Duplicate-File-in-System.py
@@ -5,8 +5,8 @@ class Solution:
             dir=path.split(" ")
             rootpath=dir[0]
             for i in range(1,len(dir)):
-                file=dir[0]+"/"+dir[i].split("(")[0]
-                content=dir[i].split("(")[1][:-1]
+                file=dir[0]+"/"+dir[i].split("(")[0] # path with the name like /root/a/1.txt
+                content=dir[i].split("(")[1][:-1] # Content of the file
                 
                 if content in dict.keys():
                     dict[content].append(file)
