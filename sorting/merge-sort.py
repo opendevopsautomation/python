3127f39d3ad8f8bd30cc77a49da0fd3ec959de5b
vikas sharma <vikas.prabhakar25@gmail.com>
2021-05-31 12:09:47 +0530

    sorting programm

---
+++ sorting/merge-sort.py
@@ -0,0 +1,31 @@
+#TC=O(nlogn) in worst case
+#SC= O(n)
+def mergesort(arr):
+    if len(arr) > 1:
+        mid=len(arr)//2
+        l=arr[:mid]
+        r=arr[mid:]
+        mergesort(l)
+        mergesort(r)
+    
+        i=j=k=0
+
+        while i < len(l) and j < len(r):
+            if l[i] < r[j]:
+                arr[k]=l[i]
+                i+=1
+            else:
+                arr[k]=r[j]
+                j+=1
+            k+=1
+        while i < len(l):
+            arr[k]=l[i]
+            i+=1
+            k+=1
+        while j < len(r):
+            arr[k]=r[j]
+            j+=1
+            k+=1    
+    return arr
+arr = [12, 11, 13, 5, 6, 7]
+print(mergesort(arr))
