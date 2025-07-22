ffa75f8e3bcb806eb44f3f1ab0a71a5835747867
vikas sharma <vikas.prabhakar25@gmail.com>
2025-07-23 00:42:17 +0530

    1695. Maximum Erasure Value

--- leetcode/Maximum-Erasure-Value.py
+++ leetcode/Maximum-Erasure-Value.py
@@ -15,3 +15,26 @@ class Solution:
                 r_sum-=nums[start]
                 start+=1
         return max_sum
+# Using two pointers and hashmap to track unique elements   
+#TC: O(n)
+#SC: O(n)
+# class Solution:
+#     def maximumUniqueSubarray(self, nums: List[int]) -> int:
+#         left,right=0,0
+#         win_sum=0
+#         max_sum=0
+#         hashmap={}
+#         while right < len(nums):
+            
+#             if nums[right] not in hashmap.keys():
+#                 hashmap[nums[right]] = True
+#                 win_sum += nums[right]
+#                 max_sum = max(win_sum, max_sum )
+#                 right+=1
+#             else:    
+#                 del hashmap[nums[left]]
+#                 win_sum -= nums[left]
+#                 left+=1
+            
+#         return max_sum
+    
