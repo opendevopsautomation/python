b7c399988418fbc9d0ea325b2bf5282e256f2bad
vikas sharma <vikas.prabhakar25@gmail.com>
2025-08-13 23:10:01 +0530

    26. Power of Three

--- leetcode/Power-of-Three.py
+++ leetcode/Power-of-Three.py
@@ -1,7 +1,16 @@
+#TC: O(log n)
+#SC: O(1)
+# Comment version TC: O(1) and SC: O(1)
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         if n <= 0:
             return False
-        lg = round(log(n,3))
-        return 3**lg == n
-        
+        # lg = round(log(n,3))
+        # return 3**lg == n
+        while n > 1:
+            remainder = n % 3
+            if remainder != 0:
+                return False
+            else:
+                n = n //3
+        return True
