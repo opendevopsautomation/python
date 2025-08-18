f8b5a38d45712acc577057100c9b5b452d7d872a
vikas sharma <vikas.prabhakar25@gmail.com>
2025-08-19 00:06:40 +0530

    2348. Number of Zero-Filled Subarrays

---
+++ leetcode/Number-of-Zero-Filled-Subarrays.py
@@ -0,0 +1,11 @@
+    class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        zero=result=0
+        for num in nums:
+            if num == 0:
+                zero+=1
+                result+=zero
+            else:
+                zero=0
+        return result
+            
